7eb649f7ae3258a7894236a60113599b62e2cb01
x23xmarcox23x <x23xmarcox23x@gmail.com>
2019-04-03 17:52:25 +0200

    aggiunto a root module reactiveform

--- src/app/Component/register/register.component.ts
+++ src/app/Component/register/register.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.sass']
 })
 export class RegisterComponent implements OnInit {
-
+  confermapassword: string
+  password: string;
   constructor() { }
 
   ngOnInit() {
   }
 
+  doRegister(user: string){
+   this.doRegister.name;
+   this.doRegister;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './Component/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
