7a8960e99abe663514e0d43c934e71787b026562
Cris2195 <cristianciarrocchi2@gmail.com>
2019-04-03 16:21:44 +0200

    Login e cose iniziali

---
+++ src/app/Service/login.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+private loginEvent$ : Subject<void> = new Subject<void>();
+public loginsEvent$ = this.loginEvent$.asObservable();
+  constructor(private router : Router) { }
+
+
+
+  doLogin(name : string , pass : string) : void{
+    if(name.length > 2 && pass.length > 4)
+    sessionStorage.setItem('user', name);
+    this.loginEvent$.next();
+    this.router.navigateByUrl('home');
+  }
+}
---
+++ src/app/loginComponent/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { getTypeNameForDebugging } from '@angular/core/src/change_detection/differs/iterable_differs';
+import { LoginService } from 'src/app/Service/login.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.sass']
+})
+export class LoginComponent implements OnInit {
+    myForm : FormGroup;
+  constructor(private fb : FormBuilder, private loginService : LoginService ) { }
+
+  ngOnInit() {
+    this.create();
+  }
+create(){
+  this.myForm = this.fb.group({
+    name : ['' , Validators.required], 
+    password : ['' , Validators.required]
+
+  })
+}
+getInputName() : FormControl{
+  return this.myForm.get('name') as FormControl;
+}
+getInputPassword() : FormControl{
+  return this.myForm.get('password') as FormControl;
+}
+doLogin(){
+this.loginService.doLogin(this.getInputName().value , this.getInputPassword().value);
+}
+}
