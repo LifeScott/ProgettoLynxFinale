ce020a9bbeec79ae63c06b61f810a1f2360a0e13
Sottob <matteobottos98@gmail.com>
2019-04-08 12:38:19 +0200

    Pagina profilo completata

--- src/app/profilo/profilo.component.ts
+++ src/app/profilo/profilo.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-profilo',
   templateUrl: './profilo.component.html',
-  styleUrls: ['./profilo.component.sass']
+  styleUrls: ['./profilo.component.scss']
 })
 export class ProfiloComponent implements OnInit {
+nome : string;
+
 
   constructor() { }
 
   ngOnInit() {
-  }
+this.retrieveData();
 
+  }
+retrieveData(){
+  this.nome = sessionStorage.getItem('user');
+
+
+}
 }
